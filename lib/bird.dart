a4c3753d32e9d0f893dc4ba5d24d4c4d687edddd
yeak8 <rgrajalescastillo@gmail.com>
2024-05-30 02:48:35 -0600

    Initial Commit

--- lib/bird.dart
+++ lib/bird.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  final birdY;
+  final double birdWidth; // normal double value for width.
+  final double birdHeight; // out of 2, 2 being the entire height of the screen
 
